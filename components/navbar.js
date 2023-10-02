0e5d4dd3f6cc17e7f83d080ff1a51aa0520b78cc
clairejyzhang <czhang.ocean@gmail.com>
2023-10-01 22:13:33 -0400

    Set up hovering animations in header

--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,29 @@
+import React, { useState } from 'react';
 import styles from './navbar.module.css';
 import Link from 'next/link';
 
+
 export default function Navbar({ children, home }) {
+    const [currentHovering, setCurrentHovering] = useState('');
+
     return (
         <section className={styles.bar}>
             <div className={styles.left}>
-                <Link className={styles.item} href={`/`}>claire zhang.</Link>
+                <NavLink href="/" text="claire zhang." currentHovering={currentHovering} setHovering={setCurrentHovering}/>
             </div>
             <div className={styles.right}>
-                <Link className={styles.item} href={`/learning`}>learning</Link>
-                <Link className={styles.item} href={`/writing`}>writing</Link>
+                <NavLink href="/learning" text="learning" currentHovering={currentHovering} setHovering={setCurrentHovering}/>
+                <NavLink href="/writing" text="writing" currentHovering={currentHovering} setHovering={setCurrentHovering}/>
             </div>
-            
         </section>
 
     );
+  }
+
+  function NavLink({href, text, currentHovering, setHovering}) {
+    return <Link onMouseEnter={() => setHovering(href)} 
+    onMouseLeave={() => setHovering('')} 
+    className={styles.item} 
+    style={currentHovering && currentHovering !== href ? {opacity: 0.5} : {opacity: 1} }
+    href={href}>{text}</Link>
   }
