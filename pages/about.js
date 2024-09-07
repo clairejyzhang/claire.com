db80c1090306e58c428714a12c56ab3e86f0cc92
Claire Zhang <czhang.ocean@gmail.com>
2024-09-07 04:48:21 -0700

    about page update

--- pages/about.js
+++ pages/about.js
@@ -41,7 +41,7 @@ export default function Home() {
             <li>Bookstores and orchestra rehearsals were my third spaces growing up</li>
             <li>Went to Columbia because I wanted to be in New York</li>
             <li>Wasn't sure what I wanted in a career when I started college so I did a bunch of random things — in hindsight I've been doing gradient descent along the axes of 'challenge,' 'impact,' and 'people' or something like that but I don't know yet if my current goal of product management is a local or global maximum</li>
-            <li>I am pretty sure what I want out of life more broadly though, which is to live in a city by the water with lots of sunny days, and people I love who'll experience them with me</li>
+            <li>I am pretty sure what I want out of life more broadly though, which is to live in a city by the water with lots of sunny days, and people I love to enjoy them with</li>
           </ul>
 
           <p>Some things I believe:</p>
@@ -49,17 +49,15 @@ export default function Home() {
             <li>The best teams are like jazz bands</li>
             <ul className={utilStyles.sublist}>
               <li>Compact, dynamic, aware, team over self, shared love of the art</li>
-              <li>Now that I think about it you could totally get all these things from playing a team sport, but I was asthmatic as a kid so no wonder I only found it through jazz</li>
             </ul>
             <li>Writing is thinking</li>
             <ul className={utilStyles.sublist}>
-              <li>Part of the purpose of this website is to serve as a forcing function for me to write... clearly it's working 🦧</li>
-              <li>Writing is 'soft power' in a workplace — things like meeting notes and weekly summaries are a privilege to send, not a chore</li>
+              <li>Making tacit knowledge explicit is soft power in a workplace — things like meeting notes and weekly summaries are a privilege to send, not a chore</li>
             </ul>
             <li>"Hell yeah or no"</li>
             <ul className={utilStyles.sublist}>
-              <li>Doing things you're excited about should give you energy instead of taking it away</li>
-              <li>Figure out what makes you feel momentum</li>
+              <li>Even doing nothing is better than doing something uninteresting</li>
+              <li>Find and maintain momentum</li>
             </ul>
             <li>Bring back analog</li>
             <ul className={utilStyles.sublist}>
