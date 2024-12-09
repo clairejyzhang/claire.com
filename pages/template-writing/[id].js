b0d61609cb8179431fac23415dcf0d6817593093
clairejyzhang <claire@mormedi.lan>
2024-12-09 15:56:34 +0100

    refactored css

---
+++ pages/template-writing/[id].js
@@ -0,0 +1,48 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/writing-script';
+import Head from 'next/head';
+import Date from '../../components/format-date';
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+
+        <article>
+
+          <div className={`${utilStyles.blogLineContainer} ${utilStyles.container55}`}>
+            <div className={utilStyles.headingMd}><b>{postData.title}</b></div>
+            <div className={`${utilStyles.container55}`}>
+              <Date dateString={postData.date} />
+            </div>
+          </div>
+         
+         <br></br>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+        
+      </Layout>
+    );
+  }
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false,
+    };
+}
+
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+    return {
+      props: {
+        postData,
+      },
+    };
+}
+  
